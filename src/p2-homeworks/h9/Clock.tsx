e5a57113188475cacf1eedca2836a38afafeb097
Pavel Vasilev <pv9998286620@yandex.ru>
2022-02-23 13:28:20 +0300

    changed style HW9

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import s from './Clock.module.css'
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0)
@@ -19,32 +20,48 @@ function Clock() {
 
     const onMouseEnter = () => {
         // show
+        setShow(true)
     }
     const onMouseLeave = () => {
         // close
+        setShow(false)
     }
 
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
+    const getTime = () => {
+        const date = new Date()
+        return `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+    }
+    const getDate = () => {
+        const today = new Date();
+        const dd = String(today.getDate()).padStart(2, '0');
+        const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+        const yyyy = today.getFullYear();
+        return dd + '.' + mm + '.' + yyyy;
+    }
+
+    const stringTime = getTime()  // fix with date
+    const stringDate = getDate() // fix with date
 
     return (
-        <div>
-            <div
-                onMouseEnter={onMouseEnter}
-                onMouseLeave={onMouseLeave}
+        <div className={s.wrapper}>
+            <div className={s.clock}
+                 onMouseEnter={onMouseEnter}
+                 onMouseLeave={onMouseLeave}
             >
-                {stringTime}
-            </div>
-
-            {show && (
-                <div>
-                    {stringDate}
+                <div className={s.label}>
+                    {stringTime}
                 </div>
-            )}
-
-            <SuperButton onClick={start}>start</SuperButton>
-            <SuperButton onClick={stop}>stop</SuperButton>
-
+            </div>
+            <div className={s.clock}>
+                {show
+                    ? <div className={s.label}>{stringDate}</div>
+                    : <div className={`${s.label}  ${s.bgGrey}`}>00:00:0000</div>
+                }
+            </div>
+            <div>
+                <SuperButton onClick={start}>start</SuperButton>
+                <SuperButton onClick={stop}>stop</SuperButton>
+            </div>
         </div>
     )
 }
