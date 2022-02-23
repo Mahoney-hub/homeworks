b55a8416651db92e8894f00bbff9314442337d01
Pavel Vasilev <pv9998286620@yandex.ru>
2022-02-23 14:14:54 +0300

    completed HW9

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -8,12 +8,13 @@ function Clock() {
     const [show, setShow] = useState<boolean>(false)
 
     const stop = () => {
-        // stop
+        clearInterval(timerId)
     }
     const start = () => {
         stop()
         const id: number = window.setInterval(() => {
             // setDate
+            setDate(new Date())
         }, 1000)
         setTimerId(id)
     }
@@ -28,15 +29,15 @@ function Clock() {
     }
 
     const getTime = () => {
-        const date = new Date()
-        return `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+        if (date) return `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
     }
     const getDate = () => {
-        const today = new Date();
-        const dd = String(today.getDate()).padStart(2, '0');
-        const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-        const yyyy = today.getFullYear();
-        return dd + '.' + mm + '.' + yyyy;
+        if (date) {
+            const dd = String(date.getDate()).padStart(2, '0');
+            const mm = String(date.getMonth() + 1).padStart(2, '0'); //January is 0!
+            const yyyy = date.getFullYear();
+            return dd + '.' + mm + '.' + yyyy;
+        }
     }
 
     const stringTime = getTime()  // fix with date
